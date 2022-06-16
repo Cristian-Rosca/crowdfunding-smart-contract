6774548194f372342a6a50446755b5991a275885
Cristian Rosca <cristianrrosca@gmail.com>
2022-06-16 22:29:35 +0100

    add state variables

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract CrowdFunding {
+    mapping(address => uint) public contributors;
+    address public admin;
+    uint public numOfContributors;
+    uint public minContribution;
+    uint public deadline; // timestamp
+    uint public goal;
+    uint public raisedAmount;
 
+    constructor(uint _goal, uint _deadline){
+        goal = _goal;
+        deadline = block.timestamp + _deadline;
+        minContribution = 100 wei;
+        admin = msg.sender;
+    }
 }
